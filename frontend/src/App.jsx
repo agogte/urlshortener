cf4970ed9244e3916f8ee97831ddb26b8c8d8dbb
agogte <agogte@gmu.edu>
2023-10-12 12:37:23 -0400

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-cyan-300 w-full h-screen flex justify-center">
+      <p className='text-5xl font-sans font-bold text-white drop-shadow-lg my-20'>Shorten it!</p>
+    </div>
+  );
+}
+
+export default App;
